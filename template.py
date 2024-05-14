d16dff395e0b41f74f847d74dd815f3b7f384cd8
Sumegh20 <sumegh20@gmail.com>
2024-05-14 10:26:33 +0530

    Add: Basic code structure

---
+++ template.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+import logging
+
+project_name = "code"
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    ".env",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb",
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+
+    file_dir, file_name = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating Directory: {file_dir} for the file: {file_name}")
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+            logging.info(f"Creating file {file_path}")
+
+    else:
+        logging.info(f"{file_name} is already exist")
